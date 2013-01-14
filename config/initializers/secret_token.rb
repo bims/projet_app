916f8d062d8c602bb1054f686bbb44d07c27381b
bims <pb402385@etu.unice.fr>
2013-01-14 11:29:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetApp::Application.config.secret_token = '1f957add755e474da50a88100c0a58a8999f1dd20e0d76834883c4e5f4b45a895ffb4deb9be6f07503cb9451b2ced9fccd61c720a5fe9090c285ffcbd893969b'
